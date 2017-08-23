6e35124c1acaad5e11f674988b4daa76335d3dec
Tim Kock <timkock@gmail.com>
2017-08-23 15:07:18 +0200

    Removed the code from jungle gpu.

--- src/batch_generator.py
+++ src/batch_generator.py
@@ -10,6 +10,62 @@ from tqdm import tqdm
 from random import shuffle
 from PIL import Image
 
+import imgaug as ia
+from imgaug import augmenters as iaa
+import numpy as np
+
+"""
+# random example images
+images = np.random.randint(0, 255, (16, 128, 128, 3), dtype=np.uint8)
+
+# Sometimes(0.5, ...) applies the given augmenter in 50% of all cases,
+# e.g. Sometimes(0.5, GaussianBlur(0.3)) would blur roughly every second image.
+sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+
+# Define our sequence of augmentation steps that will be applied to every image
+# All augmenters with per_channel=0.5 will sample one value _per image_
+# in 50% of all cases. In all other cases they will sample new values
+# _per channel_.
+seq = iaa.Sequential(
+    [
+            sometimes(iaa.Affine(
+            mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+        )),
+        # execute 0 to 5 of the following (less important) augmenters per image
+        # don't execute all of them, as that would often be way too strong
+        iaa.SomeOf((0, 5),
+            [
+                sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))), # convert images into their superpixel representation
+                iaa.OneOf([
+                    iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
+                    iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
+                    iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+                ]),
+                iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
+                iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
+                # search either for all edges or for directed edges
+                sometimes(iaa.OneOf([
+                    iaa.EdgeDetect(alpha=(0, 0.7)),
+                    iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
+                ])),
+                iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
+                iaa.OneOf([
+                    iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
+                    iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                ]),
+                iaa.Invert(0.05, per_channel=True), # invert color channels
+                iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
+                iaa.Multiply((0.5, 1.5), per_channel=0.5), # change brightness of images (50-150% of original value)
+                iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5), # improve or worsen the contrast
+                iaa.Grayscale(alpha=(0.0, 1.0)),
+                sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)), # move pixels locally around (with random strengths)
+            ],
+            random_order=True
+        )
+    ],
+    random_order=True
+)
+"""
 class CarvanaBatchGenerator():
     
     def __init__(self, mode, batch_size, dims_area=None, val_split=0.2):
@@ -62,13 +118,15 @@ class CarvanaBatchGenerator():
 
     def _get_tuple_batch_train(self, batch_ids):
         batch_images = np.concatenate([ self.pre_processed_image(_id)[np.newaxis] for _id in batch_ids])
+        # aug_batch_images = seq.augment_images(batch_images)
         batch_masks = np.concatenate([ self.pre_processed_mask(_id)[np.newaxis] for _id in batch_ids])
 
         return batch_images, batch_masks
     
     def _get_tuple_batch_test(self, batch_ids):
         batch_images = np.concatenate([ self.pre_processed_image(_id)[np.newaxis] for _id in batch_ids])
-
+        # aug_batch_images = seq.augment_images(batch_images)
+        
         return batch_images
     
     # Pre Processing Methods
@@ -81,6 +139,7 @@ class CarvanaBatchGenerator():
         #new_w = int((new_h * ratio) + 0.5)
         new_w, new_h = self.dims_area
         img = cv2.resize(img, (new_w,new_h))
+        
         img = img/255.0
         
         return img
